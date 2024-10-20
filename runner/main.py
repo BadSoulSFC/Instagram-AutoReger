db172835045e37dbbec48e35ae76cad5cc141f96
K <70845078+BadSoulSFC@users.noreply.github.com>
2024-10-20 15:59:58 +0400

    Include UC lib
    Will be using undetected chrome driver as selenium alternative

--- runner/main.py
+++ runner/main.py
@@ -1,6 +1,36 @@
-def main():
+# MIT License
+
+# Copyright (c) 2024 K
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import time
+import names
+import undetected_chromedriver as uc
+
+
+
+def main() -> None:
+    driver = uc.Chrome(headless=False, use_subprocess=False)
+    driver.get('https://www.instagram.com/accounts/emailsignup/')
+    time.sleep(5)
     pass
 
 if __name__ == "__main__":
     main()
-# end main
